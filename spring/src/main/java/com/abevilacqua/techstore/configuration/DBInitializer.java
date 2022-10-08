56753a764793f1e85b82161a06668e9e486399d1
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2022-10-08 22:26:42 +0100

    Adding @Version on Product entity to use Hibernate Optimistic Locking. Updating DBInitializer.

--- spring/src/main/java/com/abevilacqua/techstore/configuration/DBInitializer.java
+++ spring/src/main/java/com/abevilacqua/techstore/configuration/DBInitializer.java
@@ -10,11 +10,11 @@ public final class DBInitializer {
 
     public static ApplicationRunner initializeDB(ProductRepo productRepo) {
         return args -> Stream.of(
-                new Product(1, "Mouse", "Really good mouse", 20),
-                new Product(2, "Notebook", "Great notebook", 1000),
-                new Product(3, "Keyboard", "Great keyboard", 300),
-                new Product(4, "Headphones", "Great headphones", 60),
-                new Product(5, "Touchscreen", "Great screen", 500))
+                new Product(1, "Mouse", "Really good mouse", 20, 1),
+                new Product(2, "Notebook", "Great notebook", 1000, 1),
+                new Product(3, "Keyboard", "Great keyboard", 300, 1),
+                new Product(4, "Headphones", "Great headphones", 60, 1),
+                new Product(5, "Touchscreen", "Great screen", 500, 1))
                 .forEach(productRepo::save);
     }
 }
--- spring/src/main/java/com/abevilacqua/techstore/model/Product.java
+++ spring/src/main/java/com/abevilacqua/techstore/model/Product.java
@@ -2,24 +2,20 @@ package com.abevilacqua.techstore.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Builder
+@ToString
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) private long id;
     private String name;
     private String description;
     private double price;
+    @Version private long version;
 
 }
